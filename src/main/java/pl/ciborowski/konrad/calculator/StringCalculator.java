b26b8276bfe8fb673d7fae17155bc982342965b0
Konrad Ciborowski <cibor@poczta.fm>
2020-11-08 14:07:09 +0100

    Finished before final cleanup.

--- src/main/java/pl/ciborowski/konrad/calculator/StringCalculator.java
+++ src/main/java/pl/ciborowski/konrad/calculator/StringCalculator.java
@@ -1,18 +1,92 @@
 package pl.ciborowski.konrad.calculator;
 
+import static java.lang.Integer.parseInt;
+import static java.lang.String.valueOf;
+import java.util.LinkedList;
 import java.util.List;
+import static java.util.regex.Pattern.quote;
+import static java.util.stream.Collectors.joining;
 
 public class StringCalculator {
 
     static final String NEGATIVE_NUMBER_ERROR_MESSAGE = "Negative numbers not allowed: ";
-    
-    private List<String> delimiters;
-    
+
     public int add(String numbers) {
-        return -1;
+        List<String> delimiters;
+        String coreNumbersString;
+        if (numbers.startsWith("//")) {
+            int endOfDelimiters = numbers.indexOf("\\n");
+            if (endOfDelimiters < 0) {
+                throw new IllegalArgumentException("No matching delimiter marker in " + numbers);
+            }
+            delimiters = extractDelimiters(numbers.substring(0, endOfDelimiters));
+            delimiters.add("\n");
+            coreNumbersString = numbers.substring(endOfDelimiters + "\\n".length());
+        } else {
+            delimiters = List.of(",", "\n");
+            coreNumbersString = numbers;
+        }
+        return addNumbers(coreNumbersString, delimiters);     
     }
 
   
-    
+    private List<String> extractDelimiters(String delimiterString) {
+        List<String> delimiters = new LinkedList<>();
+        int leftBracketIndex = 0;
+        while ((leftBracketIndex = delimiterString.indexOf('[', leftBracketIndex)) > -1) {
+            int rightBracketIndex = delimiterString.indexOf(']', leftBracketIndex);
+            if (rightBracketIndex < 0) {
+                throw new IllegalArgumentException("Right bracket missing in " + delimiterString);
+            }
+            String delimiter = delimiterString.substring(leftBracketIndex + 1, rightBracketIndex);
+            if (delimiter.isEmpty()) {
+                throw new IllegalArgumentException("Empty delimiter in " + delimiterString);
+            }
+            if (delimiter.matches(".*\\d.*")) {
+                throw new IllegalArgumentException("Digits not allowed in delimiters in " + delimiterString);
+            }
+            delimiters.add(delimiter);
+            leftBracketIndex = rightBracketIndex + 1;
+        }
+        return delimiters;
+    }
+
+    private int addNumbers(String numbers, List<String> delimiters) {
+        if (numbers.isEmpty()) {
+            return 0;
+        }
+        StringBuilder splitter = new StringBuilder();
+        delimiters.forEach(delimiter -> {
+            if (splitter.length() > 0) {
+                splitter.append("|");
+            }
+            splitter.append(quote(delimiter));
+        });
+        String tokens[] = numbers.split(splitter.toString());
+        List<Integer> negatives = new LinkedList<>();
+        int sum = 0;
+        for (int i = 0; i < tokens.length; i++) {
+            String token = tokens[i];
+            try {
+                int number = parseInt(token);
+                if (number < 0) {
+                    negatives.add(number);
+                }
+                if (number > 1000) {
+                    number = 0;
+                }
+                sum += number;
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException(token + " is not a number in " + numbers);
+            }
+        }
+        if (negatives.isEmpty()) {
+            return sum;
+        } else{
+            StringBuilder error = new StringBuilder(NEGATIVE_NUMBER_ERROR_MESSAGE);
+            error.append(negatives.stream().map(n -> valueOf(n)).collect(joining(",")));               
+            throw new IllegalArgumentException(error.toString());
+        }
+    }
     
 }
--- src/test/java/pl/ciborowski/konrad/calculator/StringCalculatorTest.java
+++ src/test/java/pl/ciborowski/konrad/calculator/StringCalculatorTest.java
@@ -27,25 +27,28 @@ public class StringCalculatorTest {
         assertEquals(5, calculator.add("//[***]\\n5"));
         assertEquals(7, calculator.add("//[&&]\\n4&&3"));
         assertEquals(30, calculator.add("//[uue]\\n5uue10uue15"));
-        assertEquals(13, calculator.add("//[%@]\\n2\n1000%@11")); // New line us ised as the first delimiter
+        assertEquals(13, calculator.add("//[%@]\\n2\n1001%@11")); // New line us ised as the first delimiter
+        assertEquals(1013, calculator.add("//[%@]\\n2\n1000%@11")); // New line us ised as the first delimiter
     }
     
     @Test
     public void testValidCasesForMultipleDelimiters() {
         assertEquals(5, calculator.add("//[**][ff]\\n5"));
+        
         assertEquals(7, calculator.add("//[&&][**]\\n4**3"));
         assertEquals(30, calculator.add("//[uue][--]\\n5uue10--15"));
         assertEquals(23, calculator.add("//[TR][ZZ§][%@]\\n2TR1555ZZ§11\n10%@0")); // New line us ised as the first delimiter
     }
 
     @Test
-    public void testCornerCase() {
-        assertEquals(22, calculator.add("//[*][**][**]\\n5*7***10")); // One delimeter is a subset of another        
+    public void testCornerCases() {
+        assertEquals(22, calculator.add("//[***][*]\\n5*7***10")); // One delimeter is a subset of another        
+        assertThrows(IllegalArgumentException.class, () -> calculator.add("//[***][**]\\n5****6")); // First *** is applied which results is an error
     }
 
     @Test
     public void testForExceptionsForNegative() {
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> calculator.add("5,-3,10-5"));   
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> calculator.add("5,-3,10,-5"));   
         assertEquals(NEGATIVE_NUMBER_ERROR_MESSAGE + "-3,-5", exception.getMessage());
     }
         
